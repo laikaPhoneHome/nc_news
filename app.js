f8aec59c35116244a1b5c7d7de71562e2e87ad84
Robyn <clockhands9@gmail.com>
2022-10-31 09:37:24 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const apiRouter = require('./routes/api');
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api', apiRouter);
 
